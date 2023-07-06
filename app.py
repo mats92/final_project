10dead67d24e86788e2c73b5a1d076511010c879
mats92 <saulitis.matiss@gmail.com>
2023-07-06 13:42:06 +0300

    update on 06.07.

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ import datetime
 
 from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
-from flask_session import Session
 from tempfile import mkdtemp
+
+# Configure application
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    return render_template("index.html")
